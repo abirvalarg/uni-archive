af10e973b06e3ba6d082240e0e8efb8753201171
abirvalarg <yurnik2004@gmail.com>
2024-06-27 17:42:20 +0300

    base structure

---
+++ arcore/dearchive.py
@@ -0,0 +1,6 @@
+class Dearchiver:
+    def append_data(self, data: bytes):
+        raise NotImplemented()
+
+    def dump(self) -> bytes:
+        raise NotImplemented()
--- bitstream.py
+++
@@ -1,2 +0,0 @@
-class BitStream:
-    source: bytes
